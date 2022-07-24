a7b7905e90e8315416237f265aeb31459a9f4602
Cesar Menjivar <cez_r@hotmail.com>
2022-07-24 03:32:18 -0400

    Clean my files. Makefile for commit needs to be worked on

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,11 +6,7 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 15:54:43 by cemenjiv          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/07/24 02:38:09 by cemenjiv         ###   ########.fr       */
-=======
-/*   Updated: 2022/07/23 11:44:42 by cemenjiv         ###   ########.fr       */
->>>>>>> 749342b965bf20d0412c98e6309e67843163a807
+/*   Updated: 2022/07/24 03:28:05 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +18,6 @@
 # include <pthread.h>  // for all pthread functions
 # include <sys/time.h> // for gettimeof day function 
 # include <unistd.h> //for usleep function
-<<<<<<< HEAD
-=======
-# include <string.h>  // for memset
->>>>>>> 749342b965bf20d0412c98e6309e67843163a807
 
 //*** MAIN.C ***
 int	main(int argc, char **argv);
